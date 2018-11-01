d1b331623c60f6dd193b22d24bf447c27f8c0eaa
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-01 15:00:05 +0000

    Restructure files

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var rocks = document.getElementsByClassName("rock");
-    var orderArray = [];
+var rocks = document.getElementsByClassName("rock");
+console.log(rocks);
+var orderArray = [];
 
-    var rocksArray = Array.from(rocks);
-    rocksArray.forEach(function(element, index) {
-      orderArray.push(index);
-    });
+var rocksArray = Array.from(rocks);
+rocksArray.forEach(function(element, index) {
+  orderArray.push(index);
+});
 
-    var numberRocks = rocks.length;
-    var counter = numberRocks - 1;
-    rocksArray.forEach(function(element) {
-      var randomNumber = Math.floor(Math.random() * orderArray.length - 1);
-      var randomPlace = orderArray.splice(randomNumber, 1);
-      element.style = `order:${randomPlace}`;
-    });
+// rock1.style = "order:2";
+var numberRocks = rocks.length;
+var counter = numberRocks - 1;
+rocksArray.forEach(function(element) {
+  var randomNumber = Math.floor(Math.random() * orderArray.length - 1);
+  var randomPlace = orderArray.splice(randomNumber, 1);
+  element.style = `order:${randomPlace}`;
 });
